3cd8cff18b0187d9b5ce20d7a240332c92f58c63
myo-i <Imiyo0521@gmail.com>
2023-07-13 06:58:35 +0900

    ✨ waitGroup

--- main.go
+++ main.go
@@ -1,13 +1,39 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"sync"
+)
 
-func print(s string) {
+func print(s string, wg *sync.WaitGroup) {
+	defer wg.Done()
 	fmt.Println(s)
 }
 
 func main() {
+
+	var wg sync.WaitGroup
+
+	count := []string{
+		"One",
+		"Two",
+		"Three",
+		"Four",
+		"Five",
+		"Six",
+		"Seven",
+		"Eight",
+		"Nine",
+	}
+
+	wg.Add(len(count))
+	for i, x := range count {
+		go print(fmt.Sprintf("%d: %s", i, x), &wg)
+	}
+
+	wg.Wait()
+
+	wg.Add(1)
 	// Secondのみ出力
-	go print("First")
-	print("Second")
+	print("Second", &wg)
 }
