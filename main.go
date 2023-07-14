6c96fa98f0887fbc5607f430a31cd7a691443af7
myo-i <Imiyo0521@gmail.com>
2023-07-14 23:44:36 +0900

    ⚡️ mainの機能変更

--- main.go
+++ main.go
@@ -5,35 +5,36 @@ import (
 	"sync"
 )
 
-func print(s string, wg *sync.WaitGroup) {
+var msg string
+
+func updateMessage(s string) {
 	defer wg.Done()
-	fmt.Println(s)
+	msg = s
 }
+
+func printMessage() {
+	fmt.Println(msg)
+}
+
+var wg sync.WaitGroup
 
 func main() {
 
-	var wg sync.WaitGroup
-
-	count := []string{
-		"One",
-		"Two",
-		"Three",
-		"Four",
-		"Five",
-		"Six",
-		"Seven",
-		"Eight",
-		"Nine",
-	}
-
-	wg.Add(len(count))
-	for i, x := range count {
-		go print(fmt.Sprintf("%d: %s", i, x), &wg)
-	}
-
-	wg.Wait()
+	msg = "Hello, world!"
 
 	wg.Add(1)
-	// Secondのみ出力
-	print("Second", &wg)
+	go updateMessage("Hello, universe!")
+	wg.Done()
+	printMessage()
+
+	wg.Add(1)
+	go updateMessage("Hello, cosmos!")
+	wg.Done()
+	printMessage()
+
+	wg.Add(1)
+	go updateMessage("Hello, world!")
+	wg.Done()
+
+	printMessage()
 }
