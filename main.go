1f66ae710d73e700b1f49569914fc2f78e8f0d90
myo-i <Imiyo0521@gmail.com>
2023-07-16 00:44:15 +0900

    ⚡️ sync.Mutexを用いた排他ロック

--- main.go
+++ main.go
@@ -8,18 +8,22 @@ import (
 var msg string
 var wg sync.WaitGroup
 
-func updateMessage(s string) {
+func updateMessage(s string, m *sync.Mutex) {
 	defer wg.Done()
+
+	m.Lock()
 	msg = s
+	m.Unlock()
 }
 
 func main() {
-
 	msg = "Hello, world!"
 
+	var mutex sync.Mutex
+
 	wg.Add(2)
-	go updateMessage("Hello, cat")
-	go updateMessage("Hello, bird")
+	go updateMessage("Hello, cat", &mutex)
+	go updateMessage("Hello, bird", &mutex)
 	wg.Wait()
 
 	fmt.Println(msg)
