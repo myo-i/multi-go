5d5e0178c1021d1f027b865175c28d002ec297f6
myo-i <Imiyo0521@gmail.com>
2023-07-18 03:37:00 +0900

    🚧 バックグラウンドでメソッドを実行→チャネルに情報を受け取ったもとに処理を行う（大枠）

--- producer-consumer/main.go
+++ producer-consumer/main.go
@@ -1,5 +1,11 @@
 package producerconsumer
 
+import (
+	"image/color"
+	"math/rand"
+	"time"
+)
+
 const NumberOfChicken = 10
 
 var chickenMade, chickenFailed, total int
@@ -15,6 +21,31 @@ type ChickenOrder struct {
 	success       bool
 }
 
-func main() {
+func (p *Producer) Close() error {
+	ch := make(chan error)
+	p.quit <- ch
+	return <-ch
+}
 
+func kfcShop(chickenMaker *Producer) {
+	// チャネルに何かしらの情報を取得するまでは起動し続ける
+	for {
+		// 処理
+	}
+}
+
+func main() {
+	rand.Seed(time.Now().UnixNano())
+
+	color.Cyan("Open KFC!!")
+	color.Cyan("----------")
+
+	// create producer
+	kfcjob := &Producer{
+		data: make(chan ChickenOrder),
+		quit: make(chan chan error),
+	}
+
+	// run producer in the background
+	go kfcShop()
 }
