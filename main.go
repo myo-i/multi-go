925cd59b6d8093a9ec6336ad0b7eda784df48b91
myo-i <Imiyo0521@gmail.com>
2023-07-21 06:10:51 +0900

    ⚡️ consumer の作成

--- main.go
+++ main.go
@@ -2,48 +2,126 @@ package main
 
 import (
 	"fmt"
-	"sync"
+	"math/rand"
+	"time"
+
+	"github.com/fatih/color"
 )
 
-var wg sync.WaitGroup
+const NumberOfChicken = 10
 
-type Income struct {
-	Source string
-	Amount int
+var chickenMade, chickenFailed, total int
+
+type Producer struct {
+	data chan ChickenOrder
+	quit chan chan error
+}
+
+type ChickenOrder struct {
+	chickenNumber int
+	message       string
+	success       bool
+}
+
+func (p *Producer) Close() error {
+	ch := make(chan error)
+	p.quit <- ch
+	return <-ch
+}
+
+func makeChicken(chickenNumber int) *ChickenOrder {
+	chickenNumber++
+	if chickenNumber <= NumberOfChicken {
+		delay := rand.Intn(5) + 1
+		fmt.Printf("Received order number #%d\n", chickenNumber)
+
+		rnd := rand.Intn(12) + 1
+		msg := ""
+		success := false
+
+		if rnd < 5 {
+			chickenFailed++
+		} else {
+			chickenMade++
+		}
+		total++
+
+		fmt.Printf("Making chicken #%d. It will take %d seconds....\n", chickenNumber, delay)
+		time.Sleep(time.Duration(delay) * time.Second)
+
+		if rnd <= 2 {
+			msg = fmt.Sprintf("*** out of ingredients for chicken #%d", chickenNumber)
+		} else if rnd <= 4 {
+			msg = fmt.Sprintf("*** quit while making chicken #%d", chickenNumber)
+		} else {
+			success = true
+			msg = fmt.Sprintf("#%d is reafy", chickenNumber)
+		}
+
+		c := ChickenOrder{
+			chickenNumber: chickenNumber,
+			message:       msg,
+			success:       success,
+		}
+
+		return &c
+	}
+
+	return &ChickenOrder{
+		chickenNumber: chickenNumber,
+	}
+}
+
+func kfcShop(chickenMaker *Producer) {
+	// チャネルに何かしらの情報を取得するまでは起動し続ける
+	var i = 0
+	for {
+		currentChicken := makeChicken(i)
+		if currentChicken != nil {
+			i = currentChicken.chickenNumber
+			select {
+			case chickenMaker.data <- *currentChicken:
+
+			case quitChan := <-chickenMaker.quit:
+				close(chickenMaker.data)
+				close(quitChan)
+				return
+			}
+		}
+	}
 }
 
 func main() {
-	var bankBalance int
-	var balance sync.Mutex
+	// rand.Seed(time.Now().UnixNano())
+	rand.New(rand.NewSource(time.Now().UnixNano()))
 
-	fmt.Printf("Initial balance: $%d\n", bankBalance)
+	color.Cyan("KFC is open!!")
+	color.Cyan("----------")
 
-	incomes := []Income{
-		{Source: "Job", Amount: 600},
-		{Source: "Gifts", Amount: 20},
-		{Source: "Investments", Amount: 100},
-		{Source: "Part time", Amount: 30},
+	// create producer
+	kfcjob := &Producer{
+		data: make(chan ChickenOrder),
+		quit: make(chan chan error),
 	}
 
-	wg.Add(len(incomes))
+	// run producer in the background
+	go kfcShop(kfcjob)
 
-	for i, income := range incomes {
-		go func(i int, income Income) {
-			defer wg.Done()
-			for week := 1; week <= 52; week++ {
-				balance.Lock()
-				temp := bankBalance
-				temp += income.Amount
-				bankBalance = temp
-				balance.Unlock()
-
-				fmt.Printf("On week %d, Earned $%d.00, from %s\n", week, income.Amount, income.Source)
-
+	for i := range kfcjob.data {
+		if i.chickenNumber <= NumberOfChicken {
+			if i.success {
+				color.Green(i.message)
+				color.Green("Order #%d is out for delivery", i.chickenNumber)
+			} else {
+				color.Red(i.message)
+				color.Red("Can't delivery!!")
 			}
-		}(i, income)
+		} else {
+			color.Cyan("Done making chickens...")
+			err := kfcjob.Close()
+			if err != nil {
+				color.Red("Error closing channel", err)
+			}
+		}
 	}
-
-	wg.Wait()
-
-	fmt.Printf("Sum: %d", bankBalance)
 }
